4f1fc0d1813b5f7aa06d9a49419587ed20ffe465
kosachev <5616906@mail.ru>
2017-03-17 16:48:23 +0300

    implement AccountProvider

---
+++ Freelance.Provider/Context/FreelanceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Freelance.Provider.EntityModels;
+using System.Data.Entity;
+
+namespace Freelance.Provider.Context
+{
+    public class FreelanceDbContext : IdentityDbContext<User>
+    {
+        public FreelanceDbContext()
+            : base("FreelanceConnection", throwIfV1Schema: false)
+        { }
+        
+
+        public static FreelanceDbContext Create()
+        {
+            return new FreelanceDbContext();
+        }
+    }
+}
---
+++ Freelance.Provider/Providers/FreelanceSignInManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Freelance.Provider.EntityModels;
+
+
+namespace Freelance.Provider.Providers
+{
+    public class FreelanceSignInManager: SignInManager<User, string>
+    {
+        public FreelanceSignInManager(FreelanceUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(User user)
+        {
+            return user.GenerateUserIdentityAsync((FreelanceUserManager)UserManager);
+        }
+
+        public static FreelanceSignInManager Create(IdentityFactoryOptions<FreelanceSignInManager> options, IOwinContext context)
+        {
+            return new FreelanceSignInManager(context.GetUserManager<FreelanceUserManager>(), context.Authentication);
+        }
+    }
+}
